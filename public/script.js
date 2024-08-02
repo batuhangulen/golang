4f437917c9c53032792253402f2fa066c73db8e8
batuhangulen <batuhangulen78@gmail.com>
2024-08-02 17:01:59 +0300

    Version 1.0.1

---
+++ public/script.js
@@ -0,0 +1,24 @@
+const ws = new WebSocket('ws://10.56.0.217:8080/ws');
+
+ws.onopen = () => {
+    console.log('Connected to the WebSocket server');
+};
+
+ws.onmessage = (event) => {
+    const msg = JSON.parse(event.data);
+    const messagesDiv = document.getElementById('messages');
+    const messageElement = document.createElement('div');
+    messageElement.textContent = `${msg.username}: ${msg.message}`;
+    messagesDiv.appendChild(messageElement);
+};
+
+function sendMessage() {
+    const username = document.getElementById('username').value;
+    const message = document.getElementById('message').value;
+    ws.send(JSON.stringify({ username, message }));
+}
+
+function clearMessageBox() {
+    document.getElementById('message').value = '';
+
+}
